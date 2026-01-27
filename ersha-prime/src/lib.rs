519bc6d09920df57b0f31ddf9b2a1d5920744fc5
HenokNets <henoknetsanet98@gmail.com>
2026-01-28 02:14:25 +0300

    Define AppState struct for shared registry state

--- ersha-prime/src/lib.rs
+++ ersha-prime/src/lib.rs
@@ -1,3 +1,10 @@
-pub mod api;
 pub mod config;
 pub mod registry;
+pub mod api;
+
+// AppState must be defined in lib.rs to be visible to all modules
+#[derive(Clone)]
+pub struct AppState<DR, DisR> {
+    pub device_registry: DR,
+    pub dispatcher_registry: DisR,
+}
